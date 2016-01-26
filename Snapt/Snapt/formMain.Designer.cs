e996c328f2f5f790643906a23191f911e12aa9c9
Zeffe <zeffer5789@gmail.com>
2016-01-25 20:30:37 -0600

    Added form

--- Snapt/Snapt/formMain.Designer.cs
+++ Snapt/Snapt/formMain.Designer.cs
@@ -29,11 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.lblCoords = new System.Windows.Forms.Label();
+            this.niMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCoords
+            // 
+            this.lblCoords.AutoSize = true;
+            this.lblCoords.BackColor = System.Drawing.Color.White;
+            this.lblCoords.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoords.Location = new System.Drawing.Point(44, 36);
+            this.lblCoords.Name = "lblCoords";
+            this.lblCoords.Size = new System.Drawing.Size(74, 15);
+            this.lblCoords.TabIndex = 0;
+            this.lblCoords.Text = "placeHolder";
+            this.lblCoords.MouseEnter += new System.EventHandler(this.lblCoords_MouseEnter);
+            // 
+            // niMain
+            // 
+            this.niMain.BalloonTipText = "Niggers";
+            this.niMain.BalloonTipTitle = "Niggers";
+            this.niMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niMain.Icon")));
+            this.niMain.Text = "Snapt";
+            this.niMain.Visible = true;
+            this.niMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.niMain_MouseClick);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.Controls.Add(this.lblCoords);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "formMain";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formMain_FormClosing);
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.formMain_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.formMain_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCoords;
+        private System.Windows.Forms.NotifyIcon niMain;
     }
 }
 
--- Snapt/Snapt/formMain.cs
+++ Snapt/Snapt/formMain.cs
@@ -15,6 +15,88 @@ namespace Snapt
         public formMain()
         {
             InitializeComponent();
+            int id = 0;
+            RegisterHotKey(this.Handle, id, (int)KeyModifier.Alt, Keys.A.GetHashCode());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                show();
+            }
+        }
+
+        void minimized()
+        {
+            lblCoords.Hide();
+            this.TopMost = false;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        void show()
+        {
+            lblCoords.Show();
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        void updateLabel()
+        {
+            lblCoords.Location = new Point(Cursor.Position.X + 5, Cursor.Position.Y + 5);
+            lblCoords.Text = Cursor.Position.X.ToString() + ", " + Cursor.Position.Y.ToString();
+        }
+
+        private void formMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            updateLabel();
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+            minimized();
+        }
+
+        private void lblCoords_MouseEnter(object sender, EventArgs e)
+        {
+            updateLabel();
+        }
+
+        private void niMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            show();
+        }
+
+        private void formMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                minimized();
+            }
+        }
+
+        private void formMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
         }
     }
 }
